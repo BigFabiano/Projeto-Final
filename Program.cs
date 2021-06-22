ade332eb3eb9e61fe0487e7e0f1a39153d3a6bd8
FabianoEtec <nbscbs@gmail.com>
2021-06-21 23:40:00 -0300

    Concluido a primeira versão

---
+++ Program.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using Figgle;
+namespace Projeto_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            Console.Beep();
+            string opcaoUsuario = "";
+            do
+            {
+                Console.Clear();
+
+                Console.Title = "Calculadora - Versão 1.0";
+                string banner = FiggleFonts.Ogre.Render("Calculadora");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(banner);
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                Console.WriteLine("╔═══════════════════ MENU DE OPÇÃO ═══════════════════╗");
+                Console.WriteLine("║ 1.ADIÇÃO                           5.EXPONENCIAÇÃO  ║");
+                Console.WriteLine("║ 2.SUBTRAÇÃO                        6.RADICIAÇÃO     ║");
+                Console.WriteLine("║ 3.MULTIPLICAÇÃO                    7.GASTOS MENSAIS ║");
+                Console.WriteLine("║ 4.DIVISÃO                          8.SOBRE          ║");
+                Console.WriteLine("║                                    0.SAIR           ║");
+                Console.WriteLine("╚═════════════════════════════════════════════════════╝");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Escolha uma opção.: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                opcaoUsuario = Console.ReadLine();
+                Console.ResetColor();
+
+                if (opcaoUsuario == "1")
+                {
+                    Adicao();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "2")
+                {
+                    Subtracao();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "3")
+                {
+                    Multiplicacao();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "4")
+                {
+                    Divisao();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "5")
+                {
+                    Potencia();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "6")
+                {
+                    RaizQuadrada();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "7")
+                {
+                    Bonus();
+                    Console.ReadKey();
+                }
+
+                if (opcaoUsuario == "8")
+                {
+                    Sobre();
+                    Console.ReadKey();
+                }
+
+
+            } while (opcaoUsuario != "0");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("OBRIGADO POR USAR O PROGRAMA");
+            Console.ResetColor();
+
+        }
+        static void Adicao()
+        {
+            Console.WriteLine();
+            double n1, n2, resultado;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com um número....: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com outro número.: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.WriteLine();
+            resultado = n1 + n2;
+
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write($"O Resultado de {n1} + {n2} = {resultado}\n");
+            Console.ResetColor();
+
+        }
+        static void Subtracao()
+        {
+            Console.WriteLine();
+            double n1, n2, resultado;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com um número....: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com outro número.: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.WriteLine();
+            resultado = n1 - n2;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write($"O Resultado de {n1} - {n2} = {resultado}\n");
+            Console.ResetColor();
+        }
+        static void Multiplicacao()
+        {
+            Console.WriteLine();
+            double n1, n2, resultado;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com um número....: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com outro número.: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.WriteLine();
+            resultado = n1 * n2;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write($"O Resultado de {n1} * {n2} = {resultado}\n");
+            Console.ResetColor();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine();
+            double n1, n2, resultado;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com um número....: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com outro número.: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            if (n2 != 0)
+            {
+                resultado = n1 / n2;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write($"O Resultado de {n1} / {n2} = {resultado}\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Impossivel dividir por {n2}, digite um número valido.");
+                Console.ResetColor();
+            }
+
+
+        }
+        static void Potencia()
+        {
+            Console.WriteLine();
+            double n1, n2, resultado;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Entre com um número....: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write($"O nº {n1} elevado a.: ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+
+            Console.WriteLine();
+            if (n1 != 0 && n2 != 0)
+            {
+                resultado = Math.Pow(n1, n2);
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write($"O Resultado de {n1} ^ {n2} = {resultado}\n");
+                Console.ResetColor();
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Número invalido!");
+                Console.ResetColor();
+            }
+
+
+        }
+        static void RaizQuadrada()
+        {
+            Console.WriteLine();
+            double n1, raiz;
+            n1 = -1;
+
+            while (n1 < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write("Entre com um número positivo .: ");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.ResetColor();
+                if (n1 < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Valor Invalido!");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+
+            raiz = Math.Sqrt(n1);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write($"A raiz quadrada de {n1} é = {raiz}\n");
+            Console.ResetColor();
+        }
+        static void Sobre()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("╔══════════════ Projeto Final C Sharp - Calculadora V 1.0 ════════════════╗");
+            Console.ResetColor();
+            Console.WriteLine("║ Projeto final ETEC 1º Semestre. Calculadora Versão 1.0 - 20/06/2021     ║");
+            Console.WriteLine("║ onde sera aplicado todo conhecimento estudado até o momento. Calculo    ║");
+            Console.WriteLine("║ das quatro operações aritméticas básicas juntamente com mais um bonus   ║");
+            Console.WriteLine("║ soma, subtração, multiplicação, divisão, exponenciação, radiciação e ?  ║");
+            Console.WriteLine("║      Todos direitos reservados a Fabiano Nascimento de Souza ®          ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+
+        }
+        static void Bonus()
+        {
+
+            decimal luz, condominio, agua, internet, compra, gasolina, outrosGastos, total;
+
+            Console.Write("Entre com o mês desejado: ");
+            string mes = Console.ReadLine().ToUpper();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Gastos referente ao mês de {mes}");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Write("Valor da Luz R$ ");
+            luz = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Valor do condominio R$ ");
+            condominio = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Valor da água R$ ");
+            agua = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Valor da Internet R$ ");
+            internet = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Valor das compras R$ ");
+            compra = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Gasto com gasolina R$ ");
+            gasolina = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Outros gastos R$ ");
+            outrosGastos = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine();
+
+            total = luz + condominio + agua + internet + compra + gasolina + outrosGastos;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+
+            Console.WriteLine($"Seus gastos no mês de {mes} foi de {total:C}");
+            Console.ResetColor();
+
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Projeto-Final
-Calculadora Final de Semestre
+Projeto calculadora um aplicativo desenvolvido em Csharp usando tudo que foi de conhecimento no 1º Semestre na ETEC. Sera usado as operações basicas aritiméticas com um Bônus de gastos mensais.
+
+# Designer
+  ___      _            _           _
+  / __\__ _| | ___ _   _| | __ _  __| | ___  _ __ __ _ 
+ / /  / _` | |/ __| | | | |/ _` |/ _` |/ _ \| '__/ _` |
+/ /__| (_| | | (__| |_| | | (_| | (_| | (_) | | | (_| |
+\____/\__,_|_|\___|\__,_|_|\__,_|\__,_|\___/|_|  \__,_|
+
+╔═══════════════════ MENU DE OPÇÃO ═══════════════════╗
+║ 1.ADIÇÃO                           5.EXPONENCIAÇÃO  ║
+║ 2.SUBTRAÇÃO                        6.RADICIAÇÃO     ║
+║ 3.MULTIPLICAÇÃO                    7.BÔNUS          ║
+║ 4.DIVISÃO                          8.SOBRE          ║
+║                                    0.SAIR           ║
+╚═════════════════════════════════════════════════════╝
+
+## _Screenshot_
+
+![Tela do programa](Telainicial.png)
+
+- Adição
+
+![Adição](adcao.png)
+
+- Subtração
+
+![Subtração](subtracao.png)
+
+- Sobre
+
+![Sobre](sobre.png)
+
+## _Download_
+
+Baixe o arquivo abaixo. Descompacte na pasta deseja.
+
+[🎁 Download do arquivo.zip](dist/Calculadora.zip)
+
+Execute utilizando o comando:
+
+```
+dotnet Projeto-Final.dll
+```
+Ou, se você estiver no Windows, pode dar um  duplo -clique no icone do programa.
+
+## Agradecimentos
+
+- [Figgle](https://github.com/drewnoakes/figgle)
+- [Etec Adolpho Berezin](http://eteab.com.br/cms/)
+- [Prof. Ermogeneses](https://github.com/ermogenes/)
+- [Prof. Diego Neri](https://github.com/diegoneri)
+- [Dev C#](https://github.com/ermogenes/aulas-programacao-csharp)
+
+---
+
+Todos direitos reservados a Fabiano Nascimento de Souza ®
